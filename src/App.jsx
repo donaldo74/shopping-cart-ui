d85cce7e91ae06019545b48eaea43897c1037626
Gavin Coleman <gavinjcoleman@gmail.com>
2026-05-11 09:06:31 +0100

    Array showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,28 @@
+import { useState, useEffect} from 'react'
+
 const App = () => {
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/products')
+        if (!res.ok) throw new Error('Failed to fetch products')
+        const data = await res.json()
+        console.log(data)
+        setProducts(data)
+      } catch (err) {
+        setError(err.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchProducts()
+  }, [])
+
   return <div>Shopping Cart UI</div>;
 }
  
