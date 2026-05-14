e83cd4d013d90e56fd9bd8bab0d97b97775b22fb
Gavin Coleman <gavinjcoleman@gmail.com>
2026-05-14 18:40:19 +0100

    Cart Context created

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing"
 import { ProductProvider } from './context/ProductContext.jsx';
+import { CartProvider } from './context/CartContext.jsx';
 
 Sentry.init({
   dsn: "https://b4237412e9bd7fcdbd96797ea0cba2e4@o4511353910198272.ingest.de.sentry.io/4511387059093584",
@@ -16,7 +17,9 @@ Sentry.init({
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ProductProvider>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </ProductProvider>
   </StrictMode>,
 )
