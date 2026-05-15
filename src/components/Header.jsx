a161c9b3e4f3b8defe3cea929eb5f8c6cbc38d3a
Gavin Coleman <gavinjcoleman@gmail.com>
2026-05-15 19:17:50 +0100

    ok to 5:30 on video

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,23 @@
-import { FaShoppingCart } from 'react-icons/fa'
+import { useState } from "react";
 import { useCart } from "../context/CartContext";
+import { FaShoppingCart } from 'react-icons/fa'
 
 const Header = () => {
+  const [showDropdown, setShowDropdown] = useState(false)
+
   const { cart } = useCart()
   const itemCount = cart.reduce((acc, item) => acc + item.qty, 0)
-    
+  const total = cart.reduce((acc, item) => acc + item.price * item.qty, 0).toFixed(2)
   return ( 
     <header className="bg-white shadow-md p-4 flex justify-between items-center">
       <h1 className="text-2xl font-bold text-blue-600">
         Shopmate
       </h1>
       <div className="relative">
-        <FaShoppingCart className="text-2xl text-gray-700" />
+        <button
+          className="cursor-pointer"
+          onClick={() => setShowDropdown(!showDropdown)}>
+          <FaShoppingCart className="text-2xl text-gray-700" />
         {
           itemCount > 0 && (
             <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs w-5 h-5 flex items-center   justify-center rounded-full">
@@ -19,6 +25,20 @@ const Header = () => {
             </span>
           )
         }
+        </button>
+        
+        {showDropdown && (
+          <div className="absolute right-0 mt-2 w-80 bg-white border rounded shadow-lg z-50">
+            <div className="p-4">
+              <h2 className="font-semibold text-lg mb-2">Cart Items</h2>
+              {cart.length === 0 ? (
+                <p className="text-gray-500 text-sm">Your cart is empty</p>
+              ) : (
+                  <></>
+              )}
+            </div>
+          </div>
+        )}
       </div>
     </header>
   );
